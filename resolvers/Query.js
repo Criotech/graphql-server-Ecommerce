78ff28741e88119b2bab48c6e34227212795ef13
criotech <opesiyanbola8991@gmail.com>
2021-12-27 00:18:08 +0100

    filter by average rating

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,9 +1,36 @@
+const { reviews } = require("../db");
+
 exports.Query = {
     hello: (parent, args, context) => {
         return 'Hello World!'
     },
-    products: (parent, args, { products }) => {
-        return products
+    products: (parent, {filter}, { products }) => {
+        let filteredProducts = products;
+
+        if (filter) {
+            const { onSale, avgRating } = filter; 
+            if (onSale) {
+                filteredProducts = filteredProducts.filter(product => {
+                    return product.onSale
+                })
+            }
+            if ([1,2,3,4,5].includes(avgRating)) {
+                filteredProducts = filteredProducts.filter(product => {
+                    let sumRating = 0;
+                    let numberOfReviews = 0;
+                    reviews.forEach(review => {
+                        if (review.productId === product.id) {
+                            sumRating += review.rating;
+                            numberOfReviews++
+                        };
+                    });
+                    const avgProductRating = sumRating/numberOfReviews;
+
+                    return avgProductRating >= avgRating
+                })
+            }
+        }
+        return filteredProducts
     },
     product: (parent, args, { products }) => {
         const productId = args.id;
